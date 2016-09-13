98da0c2f373a9e47ae82b197eed9e31417381318
Jacob MacDonald <jaccarmac@gmail.com>
2016-09-13 15:58:31 -0700

    Hancle empty lines properly.
    (Whitespace was handled, a really empty line was not.)
    
    Address #22.

--- hy_kernel/hy_kernel.py
+++ hy_kernel/hy_kernel.py
@@ -90,7 +90,7 @@ class HyKernel(IPythonKernel):
                     if line[:2] == "%%":
                         # cell magic
                         cell.append(line)
-                    elif line[:1] in "!%":
+                    elif len(line) > 0 and line[0] in "!%":
                         # line magic
                         if chunk:
                             cell.append(self.compile_chunk(chunk))
